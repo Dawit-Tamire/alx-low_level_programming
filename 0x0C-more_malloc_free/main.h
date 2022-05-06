b10db089ee4eb773a79bf6b6c6b5538b65251570
Dawit Tamire <davtame3@gmail.com>
2022-05-06 05:00:31 -0700

    My first commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,17 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc_free directory.
- */
+#include <stdio.h>
 
 int _putchar(char c);
+void *malloc(size_t size);
+void free(void *ptr);
+void exit(int status);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endif
+#endif /* MAIN_H */
